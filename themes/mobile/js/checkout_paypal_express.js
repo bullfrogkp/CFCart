31af83fdcd1579808291b0794f257e93f4d75011
kevin <pqhitpan@gmail.com>
2016-08-22 15:13:49 -0400

    mmm

---
+++ themes/mobile/js/checkout_paypal_express.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	"use strict";
+
+	var cart = {
+		customer: {},
+		shippingAddress: {},
+		billingAddress: {},
+		products: [],
+		paymentMethod:'',
+		couponCode:'',
+		subTotal:'',
+		shippingFee:'',
+		tax:'',
+		discount:'',
+		total:'',
+		
+	};
+	
+	$(window).load(function(){
+		alert('good');
+	});
+});
