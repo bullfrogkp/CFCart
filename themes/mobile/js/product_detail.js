31c2a2a5743369f8e8aa21f3b8eed3604edd70fa
Kevin Pan <pqhitpan@gmail.com>
2016-09-06 00:16:23 -0400

    mmm

--- themes/mobile/js/product_detail.js
+++ themes/mobile/js/product_detail.js
@@ -9,4 +9,15 @@ $(function() {
 		$(".overlay-popup .attr-" + aid).hide();
 		$(".overlay-popup #attr-val-" + avid).show();
 	});
+	
+	(function main(){
+		$(function() {
+			$(".overlay-popup .detail-info-entry .entry").hide();
+			$(".overlay-popup .detail-info-entry .entry").removeClass('active');
+			
+			$(".overlay-popup .detail-info-entry").each(function( index ) {
+				  $( this ).children(".entry").first().show();
+			});
+		});
+	})();
 });
