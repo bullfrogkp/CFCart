8865c831b828e09233e6e78359094f7cef5ea521
kevin <pqhitpan@gmail.com>
2016-09-07 15:26:22 -0400

    mmm

--- themes/mobile/js/product_detail.js
+++ themes/mobile/js/product_detail.js
@@ -21,12 +21,40 @@ $(function() {
 					quantity: $("#quantity").html();
 				}
 		})
-		.done(function(data) {
-			if(data.products.length === 0) {
-				
-			} else {
-				
+		.done(function(data) {		
+			var str = '';
+			for(var i=0;i<data.products.length;i++) {
+				str += '
+				 <div class="cart-entry">
+					<a class="image"><img src="'+data.products[i].image+'" alt="'+data.products[i].name+'"></a>
+					<div class="content">
+						<a class="title" href="'+data.products[i].href+'">'+data.products[i].name+'</a>
+						<div class="quantity">Quantity: '+data.products[i].quantity+'</div>
+						<div class="price">'+data.products[i].price+'</div>
+					</div>
+					<div class="button-x"><i class="fa fa-close"></i></div>
+				</div>';
 			}
+			
+			str += '
+			<div class="summary">
+                <div class="subtotal">Subtotal: '+data.subtotal+'</div>
+                <div class="grandtotal">Grand Total <span>'+data.total+'</span></div>
+            </div>
+            <div class="cart-buttons">
+                <div class="column">
+                    <a class="button style-3" href="cart.cfm">view cart</a>
+                    <div class="clear"></div>
+                </div>
+                <div class="column">
+                    <a class="button style-4" href="checkout/checout_paypal_express.cfm">checkout</a>
+                    <div class="clear"></div>
+                </div>
+                <div class="clear"></div>
+            </div>';
+			
+			$(".popup-container").html(str);    			
+			
 		})
 		.fail(function(data) {
 			alert( "error" );
