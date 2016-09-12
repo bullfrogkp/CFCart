680f14949b331cd4abc015c7573df6b0e446eda0
kevin <pqhitpan@gmail.com>
2016-09-12 16:05:30 -0400

    mmm

--- themes/mobile/js/product_detail.js
+++ themes/mobile/js/product_detail.js
@@ -24,13 +24,13 @@ $(function() {
 				})
 				.done(function(data) {		
 					var str = '';
-					for(var i=0;i<data.products.length;i++) {
-						str += '<div class="cart-entry"><a class="image"><img src="'+data.products[i].image+'" alt="'+data.products[i].name+'"></a><div class="content"><a class="title" href="'+data.products[i].href+'">'+data.products[i].name+'</a><div class="quantity">Quantity: '+data.products[i].quantity+'</div><div class="price">'+data.products[i].price+'</div></div></div>';
+					for(var i=0;i<data.PRODUCTS.length;i++) {
+						str += '<div class="cart-entry"><a class="image"><img src="'+data.PRODUCTS[i].IMAGE+'" alt="'+data.PRODUCTS[i].NAME+'"></a><div class="content"><a class="title" href="'+data.PRODUCTS[i].HREF+'">'+data.PRODUCTS[i].NAME+'</a><div class="quantity">Quantity: '+data.PRODUCTS[i].QUANTITY+'</div><div class="price">'+data.PRODUCTS[i].PRICE+'</div></div></div>';
 					}
 					
-					str += '<div class="summary"><div class="subtotal">Subtotal: '+data.subtotal+'</div><div class="grandtotal">Grand Total <span>'+data.total+'</span></div></div><div class="cart-buttons"><div class="column"><a class="button style-3" href="cart.cfm">view cart</a><div class="clear"></div></div><div class="column"><a class="button style-4" href="checkout/checout_paypal_express.cfm">checkout</a><div class="clear"></div></div><div class="clear"></div></div>';
+					str += '<div class="summary"><div class="subtotal">Subtotal: '+data.SUBTOTAL+'</div><div class="grandtotal">Grand Total <span>'+data.TOTAL+'</span></div></div><div class="cart-buttons"><div class="column"><a class="button style-3" href="cart.cfm">view cart</a><div class="clear"></div></div><div class="column"><a class="button style-4" href="checkout/checout_paypal_express.cfm">checkout</a><div class="clear"></div></div><div class="clear"></div></div>';
 					
-					$(".popup-container").html(str);   
+					$(".cart-box .popup-container").html(str);   
 				})
 				.fail(function(data) {
 					alert( "error" );
